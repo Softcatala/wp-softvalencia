bce16f391e5ee29f43755f735c15cd18b5fb7a13
Xavi Ivars <xavi.ivars@gmail.com>
2015-03-01 22:53:44 +0100

    [twig] Implement 404 page using Timber (Twig for wordpress)

--- 404.php
+++ 404.php
@@ -1,20 +1,11 @@
-<?php get_header(); ?>
+<?php
 
-			<div id="content">
-				<div class="post">
-					<h2>No s'ha trobat la pàgina</h2><br />
-					<p>Sembla que la pàgina que estaves buscant no existeix:<br /><br />
-					<div style="font-weight:bold;text-align:center;width:100%"><pre><?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?></pre></div></p><br />
-					<?php dynamic_sidebar( '404' ); ?>
-				</div>
-			</div>
+    $context = Timber::get_context();
 
-			<div id="sidebar">
-				<?php
-					get_sidebar();
-				?>
-			</div>
+    $context['sidebar404'] = Timber::get_widgets('404');
 
-<?php get_footer(); ?>
+    $context['url'] = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 
-		
+	$context['posts'] = array();
+
+    Timber::render('twig/index.twig', $context);
